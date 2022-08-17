391c530db5d2b0fdb2c943280b0402f2d14a991b
AramAnanyan <aramo.ananyan@gmail.com>
2022-08-17 15:28:23 +0400

    Add project files.

---
+++ FileToExcel/ExcelHelper.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace FileToExcel
+{
+    
+    class ExcelHelper:IDisposable
+    {
+        private Application _excel;
+        private Workbook _workbook;
+        private string _filePath;
+
+        public ExcelHelper()
+        {
+            _excel=new Excel.Application();
+        }
+
+        public void Dispose()
+        {
+            try
+            {
+                _workbook.Close();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        internal bool Open(string filePath)
+        {
+            try
+            {
+                if (File.Exists(filePath))
+                {
+                    _workbook = _excel.Workbooks.Open(filePath);
+                }
+                else
+                {
+                    _workbook = _excel.Workbooks.Add();
+                    _filePath = filePath;
+                }
+                return true;
+            }
+            catch(Exception h)
+            {
+                Console.WriteLine(h.Message);
+            }
+            return false;
+        }
+
+        internal bool Set(string v1, int v2, object v3)
+        {
+            try 
+            {
+                ((Excel.Worksheet)_excel.ActiveSheet).Cells[v2, v1] = v3;
+                return true;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return false;
+        }
+        public void save()
+        {
+            if (!string.IsNullOrEmpty(_filePath))
+            {
+                _workbook.SaveAs(_filePath);
+                
+            }
+            else _workbook.Save();
+        }
+    }
+}
---
+++ FileToExcel/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileToExcel
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string path = "note1.txt";
+            string text = "aram,ababa,ahaha";
+            string m;
+            int e = 0, f = 0, i = 0, j = 1; ;
+            using (StreamWriter writer = new StreamWriter(path, true))
+            {
+                await writer.WriteLineAsync(text);
+                writer.Close();
+            }
+            using (StreamReader reader = new StreamReader(path))
+            {
+                m = reader.ReadToEnd();
+                reader.Close();
+            }
+            
+            
+
+
+            try 
+            {
+                using (var helper=new ExcelHelper())
+                {
+                    if(helper.Open(filePath: Path.Combine(Environment.CurrentDirectory, "test.xlsx")))
+                    {
+                        for (e = 0, f = 0, i = 0; i < m.Length; i++)
+                        {
+                            if (m[i] == ',')
+                            {
+                                f = i;
+                                helper.Set("A", j, m.Substring(e, f - e-1));
+                                j++;
+                            }
+                            e = f+1;
+                        }
+                        
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
